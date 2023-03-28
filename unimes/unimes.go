c4b4cb531cb8872212f47b3bf19f6d232825ffd8
satyshef <satyshef@gmail.com>
2023-03-28 11:20:59 +0300

    Added Metadata to Unimess struct

--- unimes/unimes.go
+++ unimes/unimes.go
@@ -4,7 +4,8 @@ import "time"
 
 // Unimes
 type UniversalMessage struct {
-	ID        int64       `json:"id"`        //message ID
+	ID        int64       `json:"id"` //message ID
+	Metadata  Metadata    `json:"metadata"`
 	Time      time.Time   `json:"time"`      //Дата получения
 	IsPublic  bool        `json:"is_public"` //Указывает доступность сообщения сторонним пользователям
 	Sender    Destination `json:"sender"`    //Создатель сообщения
@@ -13,23 +14,40 @@ type UniversalMessage struct {
 	Content   Content     `json:"content"`   //Содержание сообщения
 }
 
+type Metadata struct {
+	Status            StatusMessage   `json:"status"`        //Тип сообщения новое, пересланое, ответ на другое сообщение
+	OriginalLink      string          `json:"original_link"` //ссылка на оригинал сообщения
+	OriginalChatID    int64           `json:"original_chat_id"`
+	OriginalMessageID int64           `json:"original_message_id"`
+	OriginalChatType  TypeDestination `json:"original_chat_type"`
+	OriginalChatName  string          `json:"original_chat_name"`
+}
+
 type Content struct {
-	Type        TypeContent `json:"type"`
-	MessageLink string      `json:"msg_link"`
-	FotoLink    string      `json:"foto_link"`
-	VideoLink   string      `json:"video_link"`
-	Text        string      `json:"text"`
+	Type      TypeContent `json:"type"`
+	Link      string      `json:"link"`
+	FotoLink  string      `json:"foto_link"`
+	VideoLink string      `json:"video_link"`
+	Text      string      `json:"text"`
 }
 
 type Destination struct {
-	ID        int64       `json:"id"`
-	Type      string      `json:"type"`    //тип адрессата(пользователь, канал, группа)
-	Service   TypeService `json:"service"` //телеграм, фейсбук, файловая система
-	FirstName string      `json:"first_name"`
-	Lastname  string      `json:"last_name"`
-	Username  string      `json:"username"`
-	Phone     string      `json:"phone"`
+	ID        int64           `json:"id"`
+	Type      TypeDestination `json:"type"`    //тип адрессата(пользователь, канал, группа)
+	Service   TypeService     `json:"service"` //телеграм, фейсбук, файловая система
+	FirstName string          `json:"first_name"`
+	Lastname  string          `json:"last_name"`
+	Username  string          `json:"username"`
+	Phone     string          `json:"phone"`
 }
+
+type StatusMessage string
+
+const (
+	StatusMessageNew     StatusMessage = "new"
+	StatusMessageReply   StatusMessage = "reply"
+	StatusMessageForward StatusMessage = "forward"
+)
 
 type TypeService string
 
@@ -43,6 +61,17 @@ const (
 	TypeContentText  TypeContent = "text"
 	TypeContentPhoto TypeContent = "photo"
 	TypeContentVideo TypeContent = "video"
+)
+
+type TypeDestination string
+
+const (
+	TypeDestinationChannel        TypeDestination = "channel"
+	TypeDestinationPrivateChannel TypeDestination = "private_channel"
+	TypeDestinationGroup          TypeDestination = "group"
+	TypeDestinationPrivateGroup   TypeDestination = "private_group"
+	TypeDestinationUser           TypeDestination = "user"
+	TypeDestinationBot            TypeDestination = "bot"
 )
 
 /*
